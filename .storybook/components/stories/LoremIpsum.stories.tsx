8c3779bdf495dd217feb1a225aa4b4d9b95f6c09
Tihomir Selak <tse@futureuniverse.com>
2021-04-01 00:09:50 +0200

    PR-08: Add sample post

--- .storybook/components/stories/LoremIpsum.stories.tsx
+++ .storybook/components/stories/LoremIpsum.stories.tsx
@@ -2,12 +2,13 @@ import { Meta } from "@storybook/react";
 import React from "react";
 
 import { LoremIpum as LoremIpumPage } from "../LoremIpsum";
+import { SamplePost } from "../SamplePost";
 
 export const LoremIpum = () => <LoremIpumPage />;
+export const Post = () => <SamplePost />;
 
 const meta = {
-  title: "Tests/Page",
-  component: LoremIpumPage
+  title: "Styles/Page"
 };
 
 export default meta as Meta;
