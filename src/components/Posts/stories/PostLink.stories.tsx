91216a692b2e6b36b0a15b8278a3545f520b41ae
Tihomir Selak <tse@futureuniverse.com>
2021-04-06 00:27:58 +0200

    PR-09: Add Post Link Component

---
+++ src/components/Posts/stories/PostLink.stories.tsx
@@ -0,0 +1,55 @@
+/// <reference path="../../../definitions/images/svg.d.ts" />
+import { select, text } from "@storybook/addon-knobs";
+import { Meta } from "@storybook/react";
+import React from "react";
+
+import netwokNodesImage from "../../../assets/images/network-nodes.svg";
+import {
+  FeaturedPostLink as FeaturedPostLinkComponent,
+  PostLink as PostLinkComponent,
+  TPostLinkSizes
+} from "../PostLink";
+
+export const PostLink = () => {
+  const postTitleText = text("Post Title", "Post title text", "OPTIONS");
+
+  const sizes = ["regular", "small"];
+
+  const headingSize = select("Size", sizes, sizes[1], "OPTIONS");
+
+  return (
+    <PostLinkComponent
+      text={postTitleText}
+      to="./"
+      size={headingSize as TPostLinkSizes}
+      tags={[
+        { to: "./", text: "javascript" },
+        { to: "./", text: "node" },
+        { to: "./", text: "typescript" }
+      ]}
+    />
+  );
+};
+
+export const FeaturedPostLink = () => {
+  const postTitleText = text("Post Title", "Post title text", "OPTIONS");
+
+  return (
+    <FeaturedPostLinkComponent
+      text={postTitleText}
+      to="./"
+      imageLink={netwokNodesImage}
+      tags={[
+        { to: "./", text: "javascript" },
+        { to: "./", text: "node" },
+        { to: "./", text: "typescript" }
+      ]}
+    />
+  );
+};
+
+const meta = {
+  title: "Components/Post"
+};
+
+export default meta as Meta;
