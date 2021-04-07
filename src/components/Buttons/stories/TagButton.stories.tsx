580620eeffbf51015895895a8490090c650d7ecf
Tihomir Selak <tse@futureuniverse.com>
2021-04-08 01:00:24 +0200

    PR-12: Add wide button component

--- src/components/Buttons/stories/TagButton.stories.tsx
+++ src/components/Buttons/stories/TagButton.stories.tsx
@@ -1,14 +1,35 @@
 import { action } from "@storybook/addon-actions";
-import { text } from "@storybook/addon-knobs";
+import { boolean, text } from "@storybook/addon-knobs";
 import { Meta } from "@storybook/react";
 import React from "react";
 
-import { TagButton as Button } from "../TagButton";
+import {
+  FeaturedTagButton as FeaturedButton,
+  TagButton as Button
+} from "../TagButton";
 
 export const TagButton = () => {
   const buttonText = text("Button Text", "node", "OPTIONS");
 
-  return <Button text={buttonText} onClick={action("onClick - Tag Button")} />;
+  return (
+    <Button
+      text={buttonText}
+      onClick={action("onClick - Tag Button")}
+      active={boolean("Active", false, "OPTIONS")}
+    />
+  );
+};
+
+export const FeaturedTagButton = () => {
+  const buttonText = text("Button Text", "node", "OPTIONS");
+
+  return (
+    <FeaturedButton
+      text={buttonText}
+      onClick={action("onClick - Tag Button")}
+      active={boolean("Active", false, "OPTIONS")}
+    />
+  );
 };
 
 const meta = {
