fd4a91e96856af0f62833d27bd7f4169f54fd9bb
Tihomir Selak <tse@futureuniverse.com>
2021-04-10 00:53:13 +0200

    PR-13: Remove react-use dependecy in favor of Context

---
+++ src/store/store.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+import { initialState, reducer, TState } from "./reducers";
+
+type TSelector = (state: TState) => any;
+
+type TInitialState = {
+  state: TState;
+  dispatch: any;
+};
+
+const ContextInitialState: TInitialState = {
+  state: initialState,
+  dispatch: () => null
+};
+
+const StateContext = createContext(ContextInitialState);
+
+interface IProviderProps {
+  children: React.ReactNode;
+}
+
+const Provider = ({ children }: IProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+const useStore = () => {
+  const { state, dispatch } = useContext(StateContext);
+
+  return { state, dispatch };
+};
+
+const useSelector = (selector: TSelector) => {
+  const { state } = useStore();
+
+  return selector(state);
+};
+
+export { useStore, Provider, useSelector };
