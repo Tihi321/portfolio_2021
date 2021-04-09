1fe9e4d25ec3ece6879b20739b5d1bf5e9186528
Tihomir Selak <tse@futureuniverse.com>
2021-04-10 00:52:02 +0200

    PR-13: Fix build with ssr rendering

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,28 +1,22 @@
-const React = require("react");
+import React from "react";
 
-const { Provider } = require("./src/store");
-const { ThemeContainer } = require("./src/themes");
-const { Layout } = require("./src/components/Containers/Layout");
-const { CoreStylesClass } = require("./src/enums");
+import { StyleLayout } from "./src/components/Containers/StyleLayout";
+import { CoreStylesClass } from "./src/enums";
+import { Provider } from "./src/store";
+import { ThemeContainer } from "./src/themes";
 
-const wrapRootElement = ({ element }) => (
+export const wrapRootElement = ({ element }) => (
   <Provider>
     <ThemeContainer>{element}</ThemeContainer>
   </Provider>
 );
 
-const wrapPageElement = ({ element, props }) => (
-  <Layout {...props}>{element}</Layout>
+export const wrapPageElement = ({ element, props }) => (
+  <StyleLayout {...props}>{element}</StyleLayout>
 );
 
-const onRenderBody = ({ setBodyAttributes }) => {
+export const onRenderBody = ({ setBodyAttributes }) => {
   setBodyAttributes({
     className: CoreStylesClass
   });
-};
-
-module.exports = {
-  wrapRootElement,
-  wrapPageElement,
-  onRenderBody
 };
