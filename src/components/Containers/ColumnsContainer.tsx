71eb9eaa53eaa68f706fdfda2c724bdcc64b9d97
Tihomir Selak <tse@futureuniverse.com>
2021-04-11 20:56:12 +0200

    PR-14: Add ColumnsContainer Component

---
+++ src/components/Containers/ColumnsContainer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+import { IContainerProps } from "../../definitions";
+import { EBreakpoints } from "../../enums";
+import { media } from "../../utils";
+
+const ColumnsContainerStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px;
+
+  ${media(EBreakpoints.LAPTOP)} {
+    grid-template-columns: minmax(150px, 200px) 1fr;
+  }
+`;
+
+export const ColumnsContainer = ({ children }: IContainerProps) => (
+  <ColumnsContainerStyled>{children}</ColumnsContainerStyled>
+);
