e3ca8f75a647c05c794d07e91603b6598501ce09
Tihomir Selak <tse@futureuniverse.com>
2021-04-14 22:11:57 +0200

    PR-15: Add svg components support

--- src/definitions/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./styled";
-export * from "./components";
-export * from "./queries";
-export * from "./pages";
--- src/definitions/pages/pagination.d.ts
+++
@@ -1,7 +0,0 @@
-export interface IPostsPagination {
-  src: string;
-  limit: number;
-  skip: number;
-  current: number;
-  paged: number;
-}
