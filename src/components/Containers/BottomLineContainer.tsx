8944889b4e55f4437911d89dfe31270388f9471b
Tihomir Selak <tse@futureuniverse.com>
2021-04-17 16:26:24 +0200

    PR-18: Add Single post header

--- src/components/Containers/BottomLineContainer.tsx
+++ src/components/Containers/BottomLineContainer.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 import { bottomLineBorderColor } from "~ts/themes";
-import { IContainerProps } from "~ts/typings";
+import { IContainerProps, IStyledProps } from "~ts/typings";
+
+interface IBottomLineContainerProps extends IContainerProps, IStyledProps {}
 
 const BottomLineContainerStyled = styled.div`
   padding: 2px 0;
   border-bottom: 1px solid ${bottomLineBorderColor};
 `;
 
-export const BottomLineContainer = ({ children }: IContainerProps) => (
-  <BottomLineContainerStyled>{children}</BottomLineContainerStyled>
+export const BottomLineContainer = ({
+  children,
+  className
+}: IBottomLineContainerProps) => (
+  <BottomLineContainerStyled className={className}>
+    {children}
+  </BottomLineContainerStyled>
 );
--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,9 +1,13 @@
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
+import styled from "styled-components";
 
 import { Layout } from "~ts/components/Layout";
+import { PostHeader } from "~ts/components/Posts";
+import { EBreakpoints, ESide } from "~ts/enums";
 import { IPostPageQuery, TPostLink, TTagLink } from "~ts/typings";
+import { media } from "~ts/utils";
 
 interface IPageProps {
   data: IPostPageQuery;
@@ -14,6 +18,14 @@ interface IPageProps {
   };
 }
 
+const PostHeaderStyled = styled(PostHeader)`
+  margin-bottom: 30px;
+
+  ${media(EBreakpoints.LAPTOP, ESide.UP)} {
+    margin-bottom: 50px;
+  }
+`;
+
 const PostPage = ({ data, pageContext }: IPageProps) => {
   console.log(data);
   console.log(pageContext);
@@ -23,6 +35,12 @@ const PostPage = ({ data, pageContext }: IPageProps) => {
       title={data.post.frontmatter.title}
       description={data.post.frontmatter.excerpt}
     >
+      <PostHeaderStyled
+        title={data.post.frontmatter.title}
+        imageLink={data.post.frontmatter.thumbnail.publicURL}
+        readingTime={data.post.fields.readingTime.text}
+        tags={pageContext.tags}
+      />
       <MDXRenderer>{data.post.body}</MDXRenderer>
     </Layout>
   );
