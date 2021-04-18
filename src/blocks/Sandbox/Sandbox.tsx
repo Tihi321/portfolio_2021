eba99ac4719104acaa9c51a9c90e8ae0ded756af
Tihomir Selak <tse@futureuniverse.com>
2021-04-18 18:03:42 +0200

    PR-22: Add sandbox block

---
+++ src/blocks/Sandbox/Sandbox.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+
+import { EBreakpoints, ESide } from "~ts/enums";
+import { IStyledProps } from "~ts/typings";
+import { media } from "~ts/utils";
+
+const SandboxStyled = styled.div`
+  padding: 30px 0;
+
+  ${media(EBreakpoints.TABLET, ESide.UP)} {
+    padding: 50px 0;
+  }
+`;
+
+const IframeStyled = styled.iframe`
+  width: 100%;
+  border: 0;
+  border-radius: 4px;
+  overflow: hidden;
+  height: 58vw;
+  max-height: 500px;
+`;
+
+export type EPermissions =
+  | "geolocation"
+  | "microphone"
+  | "camera"
+  | "midi"
+  | "vr"
+  | "en";
+
+export type EIframePermissions = "allow-scripts" | "allow-same-origin";
+
+interface ISandboxProps extends IStyledProps {
+  title: string;
+  src: string;
+  style?: React.CSSProperties;
+  permissions?: EPermissions[];
+  iframePermissions?: EIframePermissions[];
+}
+
+export const Sandbox = ({
+  className,
+  title,
+  src,
+  style,
+  permissions = ["geolocation", "microphone", "camera", "midi", "vr", "en"],
+  iframePermissions = ["allow-scripts", "allow-same-origin"]
+}: ISandboxProps) => (
+  <SandboxStyled>
+    <IframeStyled
+      className={className}
+      title={title}
+      style={style && style}
+      src={src}
+      allow={permissions.join("; ")}
+      sandbox={iframePermissions.join(" ")}
+    />
+  </SandboxStyled>
+);
---
+++ src/blocks/Sandbox/stories/Sandbox.stories.tsx
@@ -0,0 +1,79 @@
+import { boolean, text } from "@storybook/addon-knobs";
+import { Meta } from "@storybook/react";
+import React from "react";
+
+import {
+  EIframePermissions,
+  EPermissions,
+  Sandbox as SandboxComponent
+} from "../Sandbox";
+
+export const Sandbox = () => {
+  const title = text("Title", "Roadtrip", "OPTIONS");
+  const source = text(
+    "Source",
+    "https://glitch.com/embed/#!/embed/svelte-roadtrip-animation?path=src/components/app.html&previewSize=50&sidebarCollapsed=true",
+    "OPTIONS"
+  );
+
+  const geolocation = boolean("geolocation", true, "OPTIONS");
+  const microphone = boolean("microphone", true, "OPTIONS");
+  const camera = boolean("camera", true, "OPTIONS");
+  const midi = boolean("midi", true, "OPTIONS");
+  const vrPermission = boolean("vr", true, "OPTIONS");
+  const enPermission = boolean("en", true, "OPTIONS");
+
+  let permissions: EPermissions[] = [];
+
+  if (geolocation) {
+    permissions = [...permissions, "geolocation"];
+  }
+
+  if (microphone) {
+    permissions = [...permissions, "microphone"];
+  }
+
+  if (camera) {
+    permissions = [...permissions, "camera"];
+  }
+
+  if (midi) {
+    permissions = [...permissions, "midi"];
+  }
+
+  if (vrPermission) {
+    permissions = [...permissions, "vr"];
+  }
+
+  if (enPermission) {
+    permissions = [...permissions, "en"];
+  }
+
+  let iframePermissions: EIframePermissions[] = [];
+
+  const scriptsPermission = boolean("allow-scripts", true, "OPTIONS");
+  const sameOriginPermission = boolean("allow-same-origin", true, "OPTIONS");
+
+  if (scriptsPermission) {
+    iframePermissions = [...iframePermissions, "allow-scripts"];
+  }
+
+  if (sameOriginPermission) {
+    iframePermissions = [...iframePermissions, "allow-same-origin"];
+  }
+
+  return (
+    <SandboxComponent
+      title={title}
+      src={source}
+      permissions={permissions}
+      iframePermissions={iframePermissions}
+    />
+  );
+};
+
+const meta = {
+  title: "Blocks/Sandbox"
+};
+
+export default meta as Meta;
